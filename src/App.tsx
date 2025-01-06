556237ede1e48787e385fd1dce8cfa1bbca90205
haroyan888 <haroyan3020@gmail.com>
2025-01-06 12:45:12 +0900

    feat: 画面の大まかな構成を作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import '@/App.css'
+import { Button } from '@/components/ui/button'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='
+      app-content 
+      h-[100vh]
+      w-[100vw]
+      my-auto
+      flex
+      flex-col
+      items-center'
+    >
+      <div className='disp-area h-[70%] flex items-center'>
+        <Button>カードを引く</Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className='control-area h-[30%] flex items-center'>
+        <Button className=''>カードを引く</Button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
